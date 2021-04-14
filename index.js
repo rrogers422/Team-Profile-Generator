e208e58adae6b52db67eefaeac14bf5383728799
Reagan Rogers <reagan422440@gmail.com>
2021-04-14 16:01:44 -0400

    Added node packages

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const inquirer = require('inquirer');
+
+const questions = 
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the Team Managers name?",
+            name: "managerName",
+        },
+        {
+            type: "input",
+            message: "What is the Mangers ID?",
+            name:"manID",
+        },
+        {
+            type: "input",
+            message:"What is the managers email address?",
+            name: "manEmail",
+        },
+        {
+            type: "input",
+            message: "What is the office number?",
+            name: "phone",
+        },
+        {
+            type: "checkbox",
+            message: "What type of Team Memeber would you like?",
+            name: "employee",
+            choices: ["Engineer", "Intern", "Nomore Team Members."],
+        },
+    ])
