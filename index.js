153507d988d2f156980a44664ce13710040523fa
rrogers422 <Reagan422440@gmail.com>
2021-06-17 11:26:49 -0400

    added manager tests

--- index.js
+++ index.js
@@ -10,28 +10,26 @@ employeeArray = [];
 
 //init to start app and Q&A
 const init = () => {
-inquirer
-.prompt([
-    {
-        type: 'input',
-        messgae: 'please enter the team name or company',
-        name: 'name',
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                messgae: 'please enter the team name or company',
+                name: 'name',
 
-    },
-])
-.then((answers) => {
-    const name = answers.name;
-    employeeArray.push(name);
+            },
+        ])
+        .then((answers) => {
+            const name = answers.name;
+            employeeArray.push(name);
 
-    managerInfo();
-});
+            managerInfo();
+        });
 };
 
 //manager callback function
 
-
-
-const managerInfo = () => 
+const managerInfo = () =>
     inquirer.prompt([
         {
             type: "input",
@@ -41,11 +39,11 @@ const managerInfo = () =>
         {
             type: "input",
             message: "What is the Mangers ID?",
-            name:"manID",
+            name: "manID",
         },
         {
             type: "input",
-            message:"What is the managers email address?",
+            message: "What is the managers email address?",
             name: "manEmail",
         },
         {
@@ -54,15 +52,16 @@ const managerInfo = () =>
             name: "phone",
         },
     ])
-    .then((answers) => {
-        const name = answers.managerName;
-        const id = answers.manID;
-        const email = answers.manEmail;
-        const phone = answers.phone;
-        const newEmployee = new Manager(name, id, email, phone);
-        employeeArray.push(newEmployee);
 
-        newEmp();
-    });
+        //consts for user answrs to be properly placed
+        .then((answers) => {
+            const name = answers.managerName;
+            const id = answers.manID;
+            const email = answers.manEmail;
+            const phone = answers.phone;
+            const newEmployee = new Manager(name, id, email, phone);
+            employeeArray.push(newEmployee);
 
-    
+            newEmp();
+        });
+
---
+++ test/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+const { TestScheduler } = require('@jest/core');
+
+
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.phone).toBe(testValue);
+});
+
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
