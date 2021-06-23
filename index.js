73a7cd9b86e0db37e3796f45e86eed1fcc08916b
rrogers422 <Reagan422440@gmail.com>
2021-06-23 13:24:41 -0400

    refactored code to push user inout into HTML doc

--- index.js
+++ index.js
@@ -164,31 +164,76 @@ const newIntern = () => {
         newEmp();
     });
     
-    function createHTML() {
-        const starterHTML = `<!DOCTYPE html>
-        <html lang ="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-            <title>Team Generator</title>
-        </head>
-        <body>
 
-        </body>
-        </html>`
-
-        fs.writeFile("./dist/team-manager.html", html, function(err) {
-            if(err) {
-                console.log(err);
-            }
-        });
-    }
-    
-    function addToHTML() {
-    
-    }
-
-};
+}
+const createHTML = () => {
+    const genArray = [];
+    const indexOne = 
+      `<!DOCTYPE html>
+       <html lang="en">
+       <head>
+          
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+           <link rel="stylesheet" href="style/style.css">
+           <meta charset="UTF-8">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <meta http-equiv="X-UA-Compatible" content="ie=edge">
+           <title>${empArray[0]}</title>
+       </head>
+       <body>
+           <div class="card-container">`
+      
+           genArray.push(indexOne);
+      for (i = 1; i < empArray.length; i++){
+          let pushed = `<div class="card" style="width: 18rem;">
+                   <div class="card-body">
+                      <h1 class="teamName">${empArray[0]}</h1>
+                       <h2 class="card-title">${empArray[i].name}</h2>
+                       <h3 class="card-subtitle mb-2 text-muted">Role: ${empArray[i].role}</h2>
+                   </div>
+                   <div class="card-bottom">
+                       <p class="card-text"> ID: ${empArray[i].id} </p>
+                       <p class="card-text"> Email: <a class="card-link" href="mailto: ${empArray[i].email}"> ${empArray[i].email}</a></p>
+              
+      `
+      if (empArray[i].officePhone) {
+          pushed += `
+          <p> Contact: ${empArray[i].officePhone}</p>
+          `
+      }
+      if (empArray[i].github) {
+          pushed += `
+          <p>GitHub: <a href="https://github.com/${empArray[i].github}">${empArray[i].github}</a></p>
+          `
+      }
+      if (empArray[i].school) {
+          pushed += `
+          <p>School: ${empArray[i].school}</p>
+          `
+      }
+      pushed += `
+      </div>
+      </div>
+      `
+      genArray.push(pushed)
+  }
+  
+  const prodFin = `
+  </div>
+  </body>
+  </html>
+  `
+  genArray.push(prodFin);
+  
+  fs.writeFile('index.html', genArray.join(""), function (err) {
+      if (err) throw err
+      else {
+          console.log("Team Generated");
+          console.log('Success');
+      }
+  })
+  }
+;
 initialize();
